5b75214d23f7d1410624c5628c8d2c37c66c85b7
Danieldanette <amaechidanfx@gmail.com>
2023-03-17 15:32:38 -0700

    101-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
-#include<unistd.h>
+
 /**
- * main - prints "and that piece of art is useful...,"
- * Return. 0
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Return. Always 1.
  */
 
 int main(void)
