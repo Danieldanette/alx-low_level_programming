afea405c7ea79d2a30a7ca14028b30e960e569c4
Danieldanette <amaechidanfx@gmail.com>
2023-03-16 21:35:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Writing a C programming lauguage with pust funtion
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzzle");
+return (0);
+}
